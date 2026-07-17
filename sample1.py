c54aeb746a9bc834176ab0a36e0fa1f7e7622b87
atulalways2 <atultewari26always@gmail.com>
2026-07-17 19:06:42 +0530

    setup.py file created

---
+++ sample1.py
@@ -0,0 +1,1 @@
+print('the sum is',2+2)
--- setup.py
+++ setup.py
@@ -1,1 +1,32 @@
-from setup import find_pakages,setup
+from setuptools import find_packages, setup
+from typing import List
+def get_requirements() -> List[str]:
+    '''
+    this function will return list of requirements
+
+    '''
+    requirement_lst:List[str]=[]
+    try:
+        with open('requirements.txt','r') as file:
+            #read  lines from the file
+            lines=file.readlines()
+            #processing each line:
+            for line in lines:
+                #stripping extra spaces
+                requirement=line.strip()
+                # ignore empty lines and -e .
+                if requirement and requirement!= '-e .':
+                    requirement_lst.append(requirement)
+    except FileNotFoundError:
+        print("requirements.txt file not found")
+
+    return requirement_lst
+
+setup(
+    name="network_security",
+    version="0.0.1",
+    author="Atul Tewari",
+    author_email="atul26tewari@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements()
+)
